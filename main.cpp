ade99b9afdfafda81ae53fb3035b681f70ba18c9
Leftvincenzo <152632273+leftvincenzo@users.noreply.github.com>
2024-02-26 15:26:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@ using namespace std;
 
 	COLOR: E = MENU
 	COLOR: c/4 = GAMEOVER
-	
+
 */
+
+string dv = "---------------", n = "Default", us = "    (+!+)\n    __|__    \n      | \n    _|-|_    ";
+char v = '|', S = ' ', ep = '0', r = 'r';
 
 //CREDITS VIEW
 static void cr() {
@@ -23,12 +26,20 @@ static void cr() {
 		" |____________________________________________________________|\n"
 		" |                                                            |\n"
 		" |  Last Update:        " << lu << "                            |\n"
-		" |  Game Version:           " << vs <<"                             |\n"
+		" |  Game Version:           " << vs << "                             |\n"
 		" |                                                            |\n"
 		" |____________________________________________________________|\n\n";
 
 	exit(0);
 
+}
+
+void pl() {
+	cout << dv << "[" << n << "]" << dv << endl;
+	cout << v << "HP:" << v << v << " " << v << "ARMA:" << r << v << S << v << "EXP:" << ep << v << endl;
+	cout << dv << dv << endl;
+	cout << us << endl;
+	cout << dv << dv << endl;
 }
 
 // GAMEOVER VIEW
@@ -41,14 +52,24 @@ static void go()
 
 	for (int i = 0; i < 2; i++)
 	{
-		Sleep(1000*1);
+		Sleep(1000 * 1);
 		system("color c");
-		Sleep(1000*1);
+		Sleep(1000 * 1);
 		system("color 4");
 	}
 
 	cr();
 
+}
+
+static void bnv() {
+	cout << ".___  ___.            _______      ___      .___  ___.  _______ \n|   \\/   |           /  _____|    /   \\     |   \\/   | |   ____|\n|  \\  /  |  ______  |  |  __     /  ^  \\    |  \\  /  | |  |__   \n|  |\\/|  | |______| |  | |_ |   /  /_\\  \\   |  |\\/|  | |   __|  \n|  |  |  |          |  |__| |  /  _____  \\  |  |  |  | |  |____ \n|__|  |__|           \\______| /__/     \\__\\ |__|  |__| |_______|\n " << endl;
+	for (int i = 0; i < 3; i++) {
+		system("color 6");
+		Sleep(1000);
+		system("color e");
+		Sleep(1000);
+	}
 }
 
 int main()
@@ -57,44 +78,33 @@ int main()
 	SetConsoleTitleA("[M-GAME]");
 
 	// DECLARATIONS
-	string ta = "N", n = "Default", dv = "---------------", us = "    (+!+)\n    __|__    \n      | \n    _|-|_    ";
-	char v = '|', S = ' ', exp='0', r = 'r';
+	string ta = "N";
 
 	do {
 
-		cout << ".___  ___.            _______      ___      .___  ___.  _______ \n|   \\/   |           /  _____|    /   \\     |   \\/   | |   ____|\n|  \\  /  |  ______  |  |  __     /  ^  \\    |  \\  /  | |  |__   \n|  |\\/|  | |______| |  | |_ |   /  /_\\  \\   |  |\\/|  | |   __|  \n|  |  |  |          |  |__| |  /  _____  \\  |  |  |  | |  |____ \n|__|  |__|           \\______| /__/     \\__\\ |__|  |__| |_______|\n " << endl;
-		for (int i = 0; i < 3; i++) {
-			system("color 6");
-			Sleep(1000);
-			system("color e");
-			Sleep(1000);
-		}
+		bnv();
 
 		cout << "Insert your nickname: ";
 		cin >> n;
 
-		Sleep(1000*3);
+		Sleep(1000 * 3);
 		system("cls");
 		system("color 0");
 
-		
+
 		system("color 1");
 
-		cout << dv << "[" << n << "]" << dv << endl;
-		cout << v << "HP:" << v << v << " " << v << "ARMA:" << r << v << S << v << "EXP:" << exp << v << endl;
-		cout << dv << dv << endl;
-		cout << us << endl;
-		cout << dv << dv << endl;
+		pl();
 
 		cout << "1) SWORD   2)   BOW    3) AXE \n";
 		cout << "Select your weapon: \n";
 		cin >> r;
 
-		switch (r) 
+		switch (r)
 		{
-			case '1': cout << "You have selected the Sword!\n"; break;
-			case '2': cout << "You have selected the Bow!\n"; break;
-			case '3': cout << "You have selected the Axe!\n"; break;
+		case '1': cout << "You have selected the Sword!\n"; break;
+		case '2': cout << "You have selected the Bow!\n"; break;
+		case '3': cout << "You have selected the Axe!\n"; break;
 		}
 
 
@@ -110,6 +120,6 @@ int main()
 		cr();
 	}
 
-    system("pause");
-    return 0;
+	system("pause");
+	return 0;
 }
